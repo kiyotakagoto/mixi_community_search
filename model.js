29ebe39c2fb576c779871b51b913351333f620df
KiyotakaGoto <spain.xavi.h@gmail.com>
2012-06-17 21:28:50 +0900

    modified that variable name is not correct

--- model.js
+++ model.js
@@ -1,7 +1,7 @@
 var community_model = {
     search : function (key) {
         var search_result = [];
-        for ( var index in communities_data ) {
+        for ( var index in this.communities_data ) {
             var name = communities_data[ index ]['name'];
             if ( name.match( /key/ ) ) {
                 search_result.push( communities_data[ index ] );
