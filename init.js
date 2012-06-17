426624abe59e8f38b0e48a46d07a3c99b53f8207
KiyotakaGoto <spain.xavi.h@gmail.com>
2012-06-17 20:48:09 +0900

    modified to debug

--- init.js
+++ init.js
@@ -1,4 +1,5 @@
 mixi_app_jsapi_wrapper.get_communities();
-$('#search_button').click( function (e) {
+var tmp = $('#search_button');
+tmp.click( function (e) {
     controller.search_on_change();
 });
