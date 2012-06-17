8a392d5532e277205772b402e0ca14449810763b
KiyotakaGoto <spain.xavi.h@gmail.com>
2012-06-17 21:30:26 +0900

    modified that variable name is not correct

--- model.js
+++ model.js
@@ -2,9 +2,9 @@ var community_model = {
     search : function (key) {
         var search_result = [];
         for ( var index in this.communities_data ) {
-            var name = communities_data[ index ]['name'];
+            var name = this.communities_data[ index ]['name'];
             if ( name.match( /key/ ) ) {
-                search_result.push( communities_data[ index ] );
+                search_result.push( this.communities_data[ index ] );
             }
         }
         return search_result;
