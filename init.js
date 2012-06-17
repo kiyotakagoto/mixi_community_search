b750e0e118e7d8301ec468e7456c2fad5715a24a
KiyotakaGoto <spain.xavi.h@gmail.com>
2012-06-17 21:24:23 +0900

    modified that wait until DOM is created

--- init.js
+++ init.js
@@ -1,7 +1,9 @@
-mixi_app_jsapi_wrapper.get_communities();
-var tmp = $('#search_form');
-var id = tmp.attr('id');
-var domlevel1 = document.getElementById('search_form');
-tmp.on('change', function (e) {
-    controller.search_on_change();
-});
+function init () {
+    mixi_app_jsapi_wrapper.get_communities();
+    var tmp = $('#search_form');
+    var id = tmp.attr('id');
+    var domlevel1 = document.getElementById('search_form');
+    tmp.on('change', function (e) {
+        controller.search_on_change();
+    });
+}
