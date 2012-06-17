206471b34ebdb05208d8866563654709b7a7e11f
KiyotakaGoto <spain.xavi.h@gmail.com>
2012-06-17 21:20:10 +0900

    modified to debug

--- init.js
+++ init.js
@@ -1,6 +1,7 @@
 mixi_app_jsapi_wrapper.get_communities();
 var tmp = $('#search_form');
 var id = tmp.attr('id');
+var domlevel1 = document.getElementById('search_form');
 tmp.on('change', function (e) {
     controller.search_on_change();
 });
