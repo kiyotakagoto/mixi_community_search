b32cae79977f7de0cf10638fff0c1fc5ce3ae580
KiyotakaGoto <spain.xavi.h@gmail.com>
2012-06-17 21:39:00 +0900

    modified to use RegExp object

--- init.js
+++ init.js
@@ -1,9 +1,6 @@
 function init () {
     mixi_app_jsapi_wrapper.get_communities();
-    var tmp = $('#search_form');
-    var id = tmp.attr('id');
-    var domlevel1 = document.getElementById('search_form');
-    tmp.on('change', function (e) {
+    $('#search_form').on('change', function (e) {
         controller.search_on_change();
     });
 }
--- model.js
+++ model.js
@@ -1,9 +1,10 @@
 var community_model = {
     search : function (key) {
         var search_result = [];
+        var reg_exp = new RegExp( key );
         for ( var index in this.communities_data ) {
             var name = this.communities_data[ index ]['name'];
-            if ( name.match( /key/ ) ) {
+            if ( name.match( reg_exp ) ) {
                 search_result.push( this.communities_data[ index ] );
             }
         }
